3a1d48fda2059c7b3931175d92a238e3e93ce186
oskartost <oskarvtost@gmail.com>
2020-02-28 13:47:51 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -56,10 +56,13 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
+		if (device.name == "CAOS") {
+			
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
 		document.getElementById("bleDeviceList").appendChild(listItem);
+		}
 }
 
 
