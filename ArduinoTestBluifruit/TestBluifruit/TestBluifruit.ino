098d327e1739150a74f0239d790b9be7ad51f691
oskartost <oskarvtost@gmail.com>
2020-03-04 13:05:33 +0100

    asdf

--- ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
+++ ArduinoTestBluifruit/TestBluifruit/TestBluifruit.ino
@@ -50,7 +50,6 @@ void error(const __FlashStringHelper*err) {
 void setup(void)
 {
   pinMode(infraredPin, INPUT);
- pinMode(0, OUTPUT);
   while (!Serial);  // required for Flora & Micro
   delay(500);
 
@@ -125,15 +124,14 @@ void loop(void)
   motionDetected = digitalRead(VIBRATION_SENSOR_PIN);
   sensorVal = analogRead(ANALOG_IN_PIN);
   int infraredRead = digitalRead(infraredPin);
-  digitalWrite(0, HIGH);
   Serial.print("infrared value: ");
   Serial.println(infraredRead);
-  //Serial.print("Analog: ");
-  //Serial.println(sensorVal);
+  Serial.print("Analog: ");
+  Serial.println(sensorVal);
   //Serial.print(" Digital: ");
   //Serial.println(motionDetected);
   //delay(500);
-  /*
+  
   char character;
   if (infraredRead == 0 && sensorVal > 950)
   {
@@ -167,6 +165,6 @@ void loop(void)
     int c = ble.read();
     Serial.print((char)c);
   }
-  */
+  
   delay(100);
 }
